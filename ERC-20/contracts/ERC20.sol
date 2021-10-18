73399d76d2d10de1adabd2a746010417f8bdf24e
Markuu-s <qwert11223381@gmail.com>
2021-10-18 20:33:48 +0300

    [Contract] Add contract and fields.

---
+++ ERC-20/contracts/ERC20.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.8.6;
+
+contract ERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) private allow;
+    address private admin;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+}
